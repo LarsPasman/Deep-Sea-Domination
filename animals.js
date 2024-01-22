b65dab54fecaa9b6d892db536643a9ea8816adbd
LarsPasman <95089151+LarsPasman@users.noreply.github.com>
2024-01-22 09:05:07 +0000

    start project en eerste dier

---
+++ animals.js
@@ -0,0 +1,18 @@
+class Animal {
+constructor() {
+  this.x = windowWidth/2;
+  this.y = windowHeight/2 + 280;
+  this.width = 50;
+  this.height = 70;
+}
+
+ display() {
+  rect(this.x, this.y, this.width, this.height);
+
+ }
+  
+  move() {
+
+   }
+  
+}
--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+let animals = [];
+
+var gameState = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
+
+  animal = new Animal(width/2, height/2, 50, 50);
 }
 
+
 function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  if (gameState == 0){
+   welkom();
+  }  
+
+  if (gameState == 1){
+   game();
+  }
+}
+
+function game(){
+  background(0);
+  
+  animal.display();
+  animal.move();
+}
+
+function mouseDragged() {
+  animal.x = mouseX;
+  animal.y = mouseY;
+  // prevent default
+  return false;
 }
