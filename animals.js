04852c94d91a904508ae67d16956d2ffc83e5735
sybroo <74767501+sybroo@users.noreply.github.com>
2024-02-05 09:02:59 +0000

    plaatjes erin gezet, collision werkt niet maar er wordt aan gewerkt

--- animals.js
+++ animals.js
@@ -7,10 +7,23 @@ constructor() {
   this.level = 1;
 }
 
+
  display() {
   rect(this.x, this.y, this.width, this.height);
 
  }
 
-  
+collision(){
+  if (
+    mouseX >= this.x &&
+    mouseX <= this.x + this.width &&
+    mouseY >= this.y &&
+    mouseY <= this.y + this.height
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+
+ }
 }
